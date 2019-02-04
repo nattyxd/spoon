bed38dca239ac2cd09f4e232373e763f097cd565
Nathaniel Baulch-Jones <xdnatty@gmail.com>
2019-02-04 15:25:34 +0000

    Did an oopsie

--- app/src/main/java/uk/ac/aston/baulchjn/mobiledev/spoon/home/RestaurantContent.java
+++ app/src/main/java/uk/ac/aston/baulchjn/mobiledev/spoon/home/RestaurantContent.java
@@ -3,6 +3,5 @@ package uk.ac.aston.baulchjn.mobiledev.spoon.home;
 public class RestaurantContent {
     // Get Data from JSON File
     private static final String JSON_URL = "googlemapsjsonapi, place API hardcoded location";
-
-    public static final
+    
 }
