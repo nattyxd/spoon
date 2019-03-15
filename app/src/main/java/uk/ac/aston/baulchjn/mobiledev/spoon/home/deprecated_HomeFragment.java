384f73605f641e300e97183a8c79395db6e8d150
Nathaniel Baulch-Jones <xdnatty@gmail.com>
2019-03-15 18:52:33 +0000

    Booking Status on home screen works

--- app/src/main/java/uk/ac/aston/baulchjn/mobiledev/spoon/home/deprecated_HomeFragment.java
+++ app/src/main/java/uk/ac/aston/baulchjn/mobiledev/spoon/home/deprecated_HomeFragment.java
@@ -1,21 +1,33 @@
 package uk.ac.aston.baulchjn.mobiledev.spoon.home;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.List;
 
+import uk.ac.aston.baulchjn.mobiledev.spoon.DatabaseHelper;
+import uk.ac.aston.baulchjn.mobiledev.spoon.FragmentStateContainer;
+import uk.ac.aston.baulchjn.mobiledev.spoon.MainActivity;
 import uk.ac.aston.baulchjn.mobiledev.spoon.R;
 
 public class deprecated_HomeFragment extends Fragment {
 
     private List<RestaurantItem> rv_list;
     private RecyclerView recyclerView;
+
+    private DatabaseHelper dbHelper;
+
+    public static TextView youHaveReservationText;
+    public static TextView youHaveReservationSubtitle;
+    public static Button youHaveReservationButton;
 
     public deprecated_HomeFragment() {
         // Required empty public constructor
@@ -26,6 +38,33 @@ public class deprecated_HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        youHaveReservationText = view.findViewById(R.id.item_reservation_notification);
+        youHaveReservationSubtitle = view.findViewById(R.id.item_reservation_notification_subtitle);
+        Button youHaveReservationButton = view.findViewById(R.id.viewReservationButton);
+
+        dbHelper = new DatabaseHelper(getContext());
+
+        final List<BookingItem> bookings = dbHelper.getAllBookingsAsList();
+
+        if(bookings == null || bookings.size() == 0){
+            youHaveReservationText.setText("There's no upcoming bookings!");
+            youHaveReservationButton.setEnabled(false);
+            youHaveReservationButton.getBackground().setTint(Color.parseColor("#c2cfd3"));
+            youHaveReservationSubtitle.setText("Go into restaurants, select a restaurant, and create a booking to make one!");
+        } else {
+            youHaveReservationText.setText("You have an upcoming booking! At " + bookings.get(0).getTimeOfBooking());
+            youHaveReservationSubtitle.setText(bookings.get(0).getNumPeopleAttending() + " attendee(s) are coming!");
+            youHaveReservationButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Bundle bundle = new Bundle();
+                    bundle.putSerializable("booking", bookings.get(0));
+                    FragmentStateContainer.getInstance().switchFragmentState(6, bundle);
+                    MainActivity.navigation.getMenu().findItem(R.id.navigation_bookings).setChecked(true);
+                }
+            });
+        }
 
 //        recyclerView = (RecyclerView) view.findViewById(R.id.home_rv);
 //
