7210fee5b9b65a42626157331c561d06fe69cefc
Nathaniel Baulch-Jones <baulchjn@aston.ac.uk>
2019-03-16 13:54:06 +0000

    Meals made fancy

--- app/src/main/java/uk/ac/aston/baulchjn/mobiledev/spoon/MealsFragment.java
+++ app/src/main/java/uk/ac/aston/baulchjn/mobiledev/spoon/MealsFragment.java
@@ -5,11 +5,16 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -35,9 +40,9 @@ import uk.ac.aston.baulchjn.mobiledev.spoon.home.MealRecyclerAdapter;
 import uk.ac.aston.baulchjn.mobiledev.spoon.home.RestaurantContent;
 
 public class MealsFragment extends Fragment {
-    private List<BookingItem> rv_list;
+    private View view;
+
     private RecyclerView recyclerView;
-    private Button sortButton;
 
     public static MealRecyclerAdapter mAdapter;
     public static TextView noMealsText;
@@ -53,14 +58,12 @@ public class MealsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        rv_list = new ArrayList<>();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_meals, container, false);
+        view = inflater.inflate(R.layout.fragment_meals, container, false);
         recyclerView = view.findViewById(R.id.meals_rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         noMealsText = view.findViewById(R.id.noMealsInfoText);
@@ -87,25 +90,44 @@ public class MealsFragment extends Fragment {
 
         MealContent.populateMeals(getContext(), mAdapter);
 
-        sortButton = view.findViewById(R.id.sortButton);
-
         dbHelper = new DatabaseHelper(getContext());
 
-        setupButtons();
+        Toolbar mToolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((MainActivity)getActivity()).setSupportActionBar(mToolbar);
+        ActionBar bar = ((MainActivity)getActivity()).getSupportActionBar();
+        bar.setTitle("Meals");
+        setHasOptionsMenu(true);
+
 //        BookingContent.jsonRequest(getActivity().getApplicationContext(), mAdapter); // prob need to replace with like BookingContent.getContent
         //rv_list = RestaurantContent.restaurantItems;
         return view;
     }
 
-    private void setupButtons(){
-        sortButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+        inflater.inflate(R.menu.meals_menu, menu);
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Toolbar mToolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((MainActivity)getActivity()).setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId()){
+            case R.id.action_sort:
                 AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getContext());
                 dialogBuilder.setTitle("Sort");
-                  dialogBuilder.setItems(R.array.en_restaurants_SortOptions, new DialogInterface.OnClickListener() {
-//                dialogBuilder.setItems(R.array.en_meals_SortOptions, new DialogInterface.OnClickListener() {
+                dialogBuilder.setItems(R.array.en_restaurants_SortOptions, new DialogInterface.OnClickListener() {
+                    //                dialogBuilder.setItems(R.array.en_meals_SortOptions, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         switch (which){
                             case 0:
@@ -133,8 +155,7 @@ public class MealsFragment extends Fragment {
                 });
 
                 dialogBuilder.create().show();
-
-            }
-        });
+        }
+        return true;
     }
 }
